aa3470d5fa5b2f913983f1018e35140cb0cc7768
Prabal2207 <prabal760@gmail.com>
2021-08-19 21:09:16 +0530

    [Prabal] Added:- Test methods to check store procedure for INSERT,DELETE,UPDATE and display by city or state

---
+++ AddressBookADO/AddressBookRepo.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AddressBookADO
+{
+    public class AddressBookRepo
+    {
+        public static string connectionString = "Server=127.0.0.1,1433;Database=AddressBook;User=sa;Password=Password123@pra#";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public bool AddContacts(AddressBookModel model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    //var qury=values()
+                    SqlCommand command = new SqlCommand("spAddAddressBook", this.connection);
+                  
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@StatementType", "INSERT");
+                    command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                    command.Parameters.AddWithValue("@LastName", model.LastName);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@State", model.State);
+                    command.Parameters.AddWithValue("@ZipCode", model.ZipCode);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@EmailId", model.EmailId);
+
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
+
+
+        public bool EditContact(AddressBookModel model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("spAddAddressBook", this.connection);
+                   
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@StatementType", "UPDATE");
+                    command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                    command.Parameters.AddWithValue("@LastName", model.LastName);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@State", model.State);
+                    command.Parameters.AddWithValue("@ZipCode", model.ZipCode);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@EmailId", model.EmailId);
+
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+
+        public bool DeleteContact(AddressBookModel model)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand("spAddAddressBook", this.connection);
+          
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@StatementType", "DELETE");
+                command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                command.Parameters.AddWithValue("@LastName", model.LastName);
+                command.Parameters.AddWithValue("@Address", model.Address);
+                command.Parameters.AddWithValue("@City", model.City);
+                command.Parameters.AddWithValue("@State", model.State);
+                command.Parameters.AddWithValue("@ZipCode", model.ZipCode);
+                command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                command.Parameters.AddWithValue("@EmailId", model.EmailId);
+
+                connection.Open();
+                var result = command.ExecuteNonQuery();
+                connection.Close();
+
+                if (result != 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+
+        public bool DisplayByCityOrState(AddressBookModel model)
+        {
+            try
+            {
+                string query = @"Select * from AddressBook where City = 'Austin'";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        model.FirstName = dr.GetString(1);
+                        model.LastName = dr.GetString(2);
+                        model.Address = dr.GetString(3);
+                        model.City = dr.GetString(4);
+                        model.State = dr.GetString(5);
+                        model.ZipCode = dr.GetString(6);
+                        model.PhoneNumber = dr.GetString(7);
+                        model.EmailId = dr.GetString(8);
+
+                        Console.WriteLine($"{model.FirstName} {model.LastName}, " +
+                            $"{model.Address}, {model.City}, {model.State}" +
+                            $", {model.ZipCode}, {model.PhoneNumber}, {model.EmailId}");
+
+                        Console.WriteLine("\n");
+
+                        dr.Close();
+                    }
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("No data found");
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+    }
+}
+
+
+
+
---
+++ AddressTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using AddressBookADO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddressTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        AddressBookRepo addressBook = new AddressBookRepo();
+        AddressBookModel model = new AddressBookModel();
+
+        [TestMethod]
+        public void AddingContactShouldReturnTrue()
+        {
+            model.FirstName = "sam";
+            model.LastName = "bat";
+            model.Address = "lank";
+            model.City = "Vegas";
+            model.State = "Texas";
+            model.ZipCode = "733301";
+            model.PhoneNumber = "1156534455";
+            model.EmailId = "sam7@xyz.com";
+
+            var result = addressBook.AddContacts(model);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void UpdatingContactShouldReturnTrue()
+        {
+            model.FirstName = "sam";
+            model.LastName = "den";
+            model.Address = "Cental park";
+            model.City = "paris";
+            model.State = "france";
+            model.ZipCode = "668899";
+            model.PhoneNumber = "88677889088";
+            model.EmailId = "sammm7@xyz.com";
+
+            var result = addressBook.EditContact(model);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void DelectingContactShouldReturnTrue()
+        {
+            model.FirstName = "Pra";
+            
+
+            var result = addressBook.DeleteContact(model);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GivenCityOrStateShouldReturnDataIfFound()
+        {
+            var result = addressBook.DisplayByCityOrState(model);
+            Assert.IsNotNull(result);
+        }
+    }
+}
